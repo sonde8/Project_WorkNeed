0da8253f899c3833e5b9acd0cab611fc2695de40
minhee3616 <minheee3616@gmail.com>
2026-01-14 10:56:05 +0900

    메일 발송 속도 개선 비동기처리

--- src/main/java/com/Workneed/workneed/Schedule/service/ScheduleMailService.java
+++ src/main/java/com/Workneed/workneed/Schedule/service/ScheduleMailService.java
@@ -7,6 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 import java.io.UnsupportedEncodingException;
@@ -21,6 +22,7 @@ public class ScheduleMailService {
     @Value("${spring.mail.username}")
     private String fromEmail;
 
+    @Async
     public void sendScheduleInviteEmail(
             String toEmail,
             String inviterName,
